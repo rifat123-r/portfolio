4969588645dcb8a1e65d1742b571120ab330dec8
unknown <tuj57607@temple.edu>
2023-12-06 11:31:06 -0500

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,327 @@
+export const Bio = {
+  name: "Main Rifat",
+  roles: [
+    "Full Stack Developer",
+    "Software Developer",
+    "Web Developer",
+    "Front-End Developer",
+    "Back-End Developer"
+  ],
+  description:
+    "As a dynamic and motivated individual, I enthusiastically welcome new challenges. I am driven by a genuine passion for learning and committed to delivering results of the highest quality. Possessing a positive attitude and embracing a growth mindset, I stand ready to make a meaningful contribution and strive for extraordinary achievements.",
+  github: "https://github.com/rifat123-r",
+  resume:
+    "https://drive.google.com/file/d/1qnBYUMbac8U9AA9Kchce3WYumuqgaY_w/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/main-rifat-71b025172/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg",
+        
+          
+      },
+      {
+        name: "C",
+        image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Framewroks",
+    skills: [
+      {
+        name: "Spring Boot",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg",
+      },
+      {
+        name: "JUnit5",
+        image:
+          "https://asset.brandfetch.io/idD7RfhCFS/id3KSPzOxb.png",
+      },
+      {
+        name: "pytest",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original-wordmark.svg",
+      },
+      {
+        name: "Django",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+      },
+      {
+        name: "JIRA",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original-wordmark.svg",
+      },
+      {
+        name: "IntelliJ",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original-wordmark.svg",
+      },
+      {
+        name: "Putty",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/putty/putty-original.svg",
+      },
+      {
+        name: "VSCode",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      
+    ],
+  },
+  {
+    title: "DevOps Tools",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+      },
+      {
+        name: "GCP",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original-wordmark.svg",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Kubernetes,",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain-wordmark.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-9ece5.appspot.com/o/temple.svg.png?alt=media&token=31cb6f97-0cf3-45eb-9194-0dca281d4afd",
+    role: "Peer Tutor, Program Design and Abstraction",
+    company: "Temple University",
+    date: "January 2021 - May 2021",
+    desc: "As a Peer Tutor for the Program Design and Abstraction course at Temple University, I supported over 20 students through one-on-one guidance, email support, and feedback on coding assignments",
+    skills: [
+      "Communication Skills",
+      "Problem-Solving",
+      "Adaptability",
+      "Code Review Skills",
+      "Positive Collaboration",
+      "Time Management",
+      "Critical Thinking",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-9ece5.appspot.com/o/temple.svg.png?alt=media&token=31cb6f97-0cf3-45eb-9194-0dca281d4afd",
+    school: "Temple University",
+    date: "Aug 2018 - Dec 2023",
+    desc: "College of Science & Technology, Philadelphia, PA",
+    degree: "Bachelor of Science in Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-9ece5.appspot.com/o/download.png?alt=media&token=08561410-301e-4534-a656-17e7f3777d81",
+    school: "University Of Maryland",
+    date: "Aug 2024 - Dec 2026",
+    desc: "I will commence my master's degree at the end of the summer, and during this period, I plan to enroll in one class per semester while simultaneously working full-time for a tech company",
+    degree: "Master of Science in Software Engineering",
+  },
+  
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Smart Mirror",
+    date: "August 2023 – December 2023",
+    description: "The Smart Mirror and Companion App system is a unique combination of hardware and software that is designed to upgrade the concept of a mirror into an adaptable and interactive smart home device. Our Smart Mirror app integrates hardware, including a Raspberry Pi, two-way mirror, display panel, camera, and microphone, with a software stack featuring Python and Java. The mirror's functions are controlled by a custom application on the Raspberry Pi, which also enables user interactions. Voice recognition technology like Azure is integrated with voice commands. We incorporate the Open Weather API to display real-time weather information on the mirror, providing a seamless and user-friendly experience. In addition to this, we utilize Next.js for front-end development, creating a dynamic and responsive UI with real-time updates and interactive features.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-9ece5.appspot.com/o/sm.png?alt=media&token=e38aa2e5-394b-425c-99c7-dd2ff60bcea2",
+    tags: [
+      "Raspberry Pi",
+      "Next.js",
+      "Python",
+      "Teamwork",
+      "Agile",
+      "CI/CD",
+      "React",
+      "TypeScript",
+      "SuperBase",
+    ],
+    category: "web app",
+    github: "https://github.com/rifat123-r/project-smart-mirror",
+    webapp: "https://capstone-projects-2023-fall.github.io/project-smart-mirror/",
+  },
+  {
+    id: 0,
+    title: "Online Job Portal",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "This project serves as a comprehensive online job search and recruitment platform, enabling users to effortlessly search for job openings, create job listings, and manage job postings. Key features encompass job search capabilities, user registration and authentication, job posting functionality, user profiles, application management, notifications, search filters, user dashboards, a messaging system, an admin panel, analytics and reporting tools, mobile responsiveness, and robust security measures.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-9ece5.appspot.com/o/job.png?alt=media&token=8f97e749-d042-4c57-96b7-b597b864917e",
+    
+      tags: [
+      "Java", 
+      "JSP", 
+      "MySQL", 
+      "Tomcat", 
+      "CSS & Bootstrap"],
+
+    category: "web app",
+    github: "https://github.com/rifat123-r/Online-Job-Portal",
+    webapp: "https://github.com/rifat123-r/Online-Job-Portal",
+  },
+  {
+    id: 1,
+    title: "Passion of the Pirates",
+    date: "January 2023 – May 2023",
+    description:
+      "A Battle Royale style pirate-themed video game where players control their pirate and sail to islands to battle zombies and reach higher endgame scores. Manage your health, sail the seas, and defeat the zombies. Developed in Unreal Engine 4",
+    image:
+      "https://user-images.githubusercontent.com/112338853/234425583-665887d8-82d5-456c-93a5-094d8bbc3084.png",
+    tags: [
+      "Unreal Engine",
+      "Game Design and Development",
+      "Agile",
+      "Teamwork",
+      ],
+    category: "android app",
+    github: "https://github.com/rifat123-r/02-Passion-of-the-Pirates-Game",
+    webapp: "https://github.com/dariodaguanno/02-Passion-of-the-Pirates-Game/releases/tag/v1.0.4.1",
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
